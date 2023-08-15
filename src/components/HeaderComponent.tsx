d742b1bc4ecb7356c8e845c0137878498a82f8f2
aydosjs <aji3385@gmail.com>
2023-08-15 20:47:25 +0500

    burger menu

--- src/components/HeaderComponent.tsx
+++ src/components/HeaderComponent.tsx
@@ -1,16 +1,39 @@
+import { useState } from "react";
+import SideBarComponent from "./SideBarComponent";
+
 export default function HeaderComponent() {
+
+  const [open, setOpen] = useState<boolean>(false)
+
   return (
-    <div className="flex flex-row items-center justify-between">
+    <div>
+
+      <div className="flex flex-row items-center justify-between flex-nowrap">
+        <div>
+          <h1 className="text-3xl font-semibold">
+            Notes
+          </h1>
+        </div>
+        <div
+          onClick={() => setOpen(!open)}
+          className="p-2 rounded-lg cursor-pointer hover:scale-90">
+          <svg xmlns="http://www.w3.org/2000/svg" className="w-7 h-7" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7" />
+          </svg>
+        </div>
+      </div>
+
+
+      {/* slide bar */}
       <div>
-        <h1 className="text-3xl font-semibold">
-          Notes
-        </h1>
+        <div
+          onClick={() => setOpen(!open)}
+          className={`${open ? 'absolute' : 'hidden'}  top-0 left-0 z-40 w-screen h-screen bg-black opacity-20`} />
+        <div className={`absolute top-0 ${open ? 'left-0' : 'left-[-106px]'} z-50 bg-white w-[106px] h-full p-4 pt-6`}>
+          <SideBarComponent />
+        </div>
       </div>
-      <div className="p-2 rounded-lg cursor-pointer hover:scale-90">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-8 h-8 text-black">
-          <path fill-rule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10zm0 5.25a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75a.75.75 0 01-.75-.75z" clip-rule="evenodd" />
-        </svg>
-      </div>
-    </div>
+
+    </div >
   )
 }
